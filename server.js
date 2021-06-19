2136b31e122fea92084bf54d789dc6d3ffcba54b
amehinansary <amehinansary@outlook.com>
2021-06-19 10:02:57 +0200

    dist/CRUDTas-error

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/crudtask/'));
+app.use(express.static('./dist/CRUDTask/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/crudtask/'}),
+    res.sendFile('index.html', {root: 'dist/CRUDTask/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 9090);
